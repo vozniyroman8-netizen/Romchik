5c3f65d5087c045888e6fab4a633f4b4bbd8597e
Roman <vozniyroman8@gmail.com>
2025-09-22 22:06:38 +0300

    lesson1

---
+++ 3th lesson/firstTask.js
@@ -0,0 +1,25 @@
+console.log(typeof 0)
+console.log(typeof undefined)
+console.log(typeof true)
+console.log(typeof "hello")
+console.log(typeof {})
+console.log(typeof null)
+
+
+
+
+
+alert("Лошарік");
+
+let con1 = prompt("Сюда");
+let con2 = prompt("туда");
+let con3 = prompt("караганда");
+
+alert(`Вот: ${con1} ${con2} ${con3}.`)
+
+
+
+
+let kaka = 12345
+let f1 = 12345 / 12345
+let f2 
